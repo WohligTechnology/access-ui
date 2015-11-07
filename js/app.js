3d9548d403940f50c3783816f64c7528ccb1469f
Pooja <pthakare33@gmail.com>
2015-11-07 11:39:35 +0530

    description added

--- js/app.js
+++ js/app.js
@@ -224,6 +224,34 @@ firstapp.directive('youtube', function($sce) {
     };
 });
 
+firstapp.filter('cut', function() {
+  return function(value, wordwise, max, tail) {
+    if (!value) return '';
+
+    max = parseInt(max, 10);
+    if (!max) return value;
+    if (value.length <= max) return value;
+
+    value = value.substr(0, max);
+    if (wordwise) {
+      var lastspace = value.lastIndexOf(' ');
+      if (lastspace != -1) {
+        value = value.substr(0, lastspace);
+      }
+    }
+
+    return value + (tail || ' …');
+  };
+});
+
+firstapp.filter('rawHtml', ['$sce',
+  function($sce) {
+    return function(val) {
+      return $sce.trustAsHtml(val);
+    };
+  }
+]);
+
 
 firstapp.directive('img', function($compile, $parse) {
     return {
